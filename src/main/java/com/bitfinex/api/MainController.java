f930bf0ffde9c39675db6dbab99dc0a896e73bf5
mehdi <m.hafidi85@gmail.com>
2021-06-20 22:13:42 -0700

    - update core bot

--- src/main/java/com/bitfinex/api/MainController.java
+++ src/main/java/com/bitfinex/api/MainController.java
@@ -1,6 +1,9 @@
 package com.bitfinex.api;
 
 
+import com.bitfinex.core.MainControllerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main-control")
 public class MainController
 {
+    private final MainControllerService mainControllerService;
+    @Autowired
+
+    public MainController(MainControllerService mainControllerService) {
+        this.mainControllerService = mainControllerService;
+    }
+    @GetMapping("/startBot")
+    public int startBot()
+    {
+        return mainControllerService.startStrategy();
+    }
 
 }
